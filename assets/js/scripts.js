26885a9a42578623d0ed6439d35fa1c5dc5a222f
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-03-09 20:50:37 +0700

    deleting file not using

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,20 @@
 // owl carousel
-$(document).ready(function() {
-	$('.owl-carousel').owlCarousel({
-		loop:true,
-		margin:30,
-		nav:false,
-		dots:false,
-		responsive:{
-			0:{
-				items:1
-			},
-			600:{
-	            items:1
-	        },
-	        1000:{
-	            items:2
-	        }
-	    }
-	});
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
 });
