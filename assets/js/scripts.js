5971c24519120938ccfc17e023794e4aa88700e0
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-03-18 13:00:57 +0700

    add skema

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -39,3 +39,45 @@ $(document).ready(function () {
     },
   });
 });
+
+// owl carousel
+$(document).ready(function () {
+  $("#skema.owl-carousel").owlCarousel({
+    loop: false,
+    margin: 30,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
+});
+
+// change button carousel index video
+$(document).ready(function () {
+  $(".testimoni__body .owl-nav button span").remove();
+  $(".testimoni__body .owl-nav .owl-prev").append(
+    '<i aria-label="Previous" class="fas fa-chevron-left"></i>'
+  );
+  $(".testimoni__body .owl-nav .owl-next").append(
+    '<i aria-label="Next" class="fas fa-chevron-right"></i>'
+  );
+});
+
+$(document).ready(function () {
+  $(".skema__body .owl-nav button span").remove();
+  $(".skema__body .owl-nav .owl-prev").append(
+    '<i aria-label="Previous" class="fas fa-chevron-left"></i>'
+  );
+  $(".skema__body .owl-nav .owl-next").append(
+    '<i aria-label="Next" class="fas fa-chevron-right"></i>'
+  );
+});
